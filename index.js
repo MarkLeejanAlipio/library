d236aaaed858aa89f7cad1a43fe195736d7bf3dd
Lee <149314728+MarkLeejanAlipio@users.noreply.github.com>
2026-05-28 14:15:48 +0800

    feat: add core functions add array that stores the books and functions for creating book obj and pushing it to the arr with random id for each book obj

---
+++ index.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, readStatus, bookId) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+    this.id = bookId;
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
+    };
+}
+
+function addBookToLibrary(title, author, pages, readStatus) {
+    const bookId = crypto.randomUUID();
+    const createBook = new Book(title, author, pages, readStatus, bookId);
+    myLibrary.push(createBook);
+}
